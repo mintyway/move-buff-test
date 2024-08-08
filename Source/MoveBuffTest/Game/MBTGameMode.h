d9b3727df9122c7b4bed2918e06746369de639c1
mintyway0112 <mintyway0112@gmail.com>
2024-08-09 00:15:09 +0900

    init

---
+++ Source/MoveBuffTest/Game/MBTGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MBTGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMBTGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMBTGameMode();
+};
+
+
+
